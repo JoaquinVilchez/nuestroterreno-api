5a4e1db349cd30d3a1a1d58010d9fe90cbb344e6
Joaquin Vilchez <joaquinvilche95@gmail.com>
2024-07-27 20:37:15 -0300

    FIX: Problem with enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TYPEORM_CONFIG } from './config/constants';
 import { ParticipantModule } from './participants/participant.module';
 import databaseConfig from './config/database.config';
-import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -19,12 +18,6 @@ import * as Joi from 'joi';
     ConfigModule.forRoot({
       isGlobal: true, // Hace que la configuración esté disponible globalmente en toda la aplicación.
       load: [databaseConfig], // Carga la configuración desde los archivos y/o funciones proporcionadas.
-      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`, // Especifica la ruta del archivo de variables de entorno.
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production') // Define los valores válidos para NODE_ENV.
-          .default('development'), // Establece un valor predeterminado si NODE_ENV no está definido.
-      }),
     }),
     ParticipantModule,
   ],
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,34 +1,34 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { join } from 'path';
 
 // Función que define y devuelve las opciones de configuración para TypeORM
 function typeormModuleOptions(): TypeOrmModuleOptions {
   return {
-    type: 'mysql', // Tipo de base de datos
-    host: process.env.DATABASE_HOST, // Host de la base de datos desde variables de entorno
-    port: parseInt(process.env.DATABASE_PORT, 10), // Puerto de la base de datos, convertido a entero
-    username: process.env.DATABASE_USERNAME, // Usuario de la base de datos desde variables de entorno
-    password: process.env.DATABASE_PASSWORD, // Contraseña de la base de datos desde variables de entorno
-    database: process.env.DATABASE_NAME, // Nombre de la base de datos desde variables de entorno
-
-    // Ubicación de las entidades, usadas por TypeORM para mapear las tablas de la base de datos
-    entities: [join(__dirname, '../**/**/*entity{.ts,.js}')],
-
-    // Opciones adicionales para TypeORM
-    autoLoadEntities: true, // Carga automáticamente las entidades en el módulo de TypeORM
-    migrationsRun: true, // Ejecuta las migraciones automáticamente
-    migrations: [join(__dirname, '../migration/**/*{.ts,.js}')], // Ubicación de las migraciones
-    migrationsTableName: 'migrations_typeorm', // Nombre de la tabla que guarda el historial de migraciones
-
-    // Opciones de sincronización y logging
-    synchronize: false, // No sincroniza automáticamente la base de datos con las entidades (se recomienda deshabilitar en producción)
-    logging: true, // Activa el logging de consultas y otras operaciones
-    logger: 'file', // Usa el sistema de archivos para guardar los logs
+    type: 'mysql',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
+    autoLoadEntities: true,
+    migrationsRun: false,
+    migrations: [join(__dirname, '../src/migrations/*{.ts,.js}')],
+    migrationsTableName: 'migrations_typeorm',
+    synchronize: false,
+    logging: true,
+    logger: 'file',
   };
 }
 
 // Exporta la configuración usando `registerAs` de @nestjs/config
 export default registerAs('database', () => ({
-  config: typeormModuleOptions(), // Llama a la función que devuelve las opciones de configuración
+  config: typeormModuleOptions(),
 }));
+
+// Crea y exporta una instancia de DataSource
+export const AppDataSource = new DataSource(
+  typeormModuleOptions() as DataSourceOptions,
+);
