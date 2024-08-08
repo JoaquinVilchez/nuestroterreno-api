68b8e6ccd9a69940cd054879b395c4d785962a24
Joaquin Vilchez <joaquinvilche95@gmail.com>
2024-08-08 14:05:09 -0300

    UPDATE: Migration entity error

--- src/results/entities/result.entity.ts
+++ src/results/entities/result.entity.ts
@@ -1,5 +1,5 @@
-import { Lot } from 'src/lots/entities/lot.entity';
 import { Participant } from 'src/participants/entities/participant.entity';
+import { Lot } from 'src/lots/entities/lot.entity';
 import {
   Column,
   CreateDateColumn,
@@ -16,31 +16,37 @@ export class Result {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ name: 'order_number', type: 'integer', nullable: true })
-  orderNumber: number;
-
   @Index()
   @Column({ name: 'group', type: 'integer' })
   group: number;
 
   @Index()
-  @Column({ name: 'draw_type', type: 'varchar', length: 50 })
+  @Column({ name: 'draw_type', type: 'varchar', length: 100 })
   drawType: string;
 
-  @Column({ name: 'result_type', type: 'varchar', length: 50 })
-  resultType: string;
-
-  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp', nullable: true })
   createdAt: Date;
 
-  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp', nullable: true })
   updatedAt: Date;
 
-  @OneToOne(() => Participant, (participant: Participant) => participant.result)
-  @JoinColumn({ name: 'participant_id' })
-  participant: Participant;
-
-  @OneToOne(() => Lot, (lot: Lot) => lot.result, { nullable: true })
-  @JoinColumn({ name: 'lot_id' })
+  @OneToOne(() => Lot, (lot: Lot) => lot.result, {
+    cascade: true,
+    onDelete: 'CASCADE',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'lot' })
   lot?: Lot;
+
+  @OneToOne(
+    () => Participant,
+    (participant: Participant) => participant.result,
+    {
+      cascade: true,
+      onDelete: 'CASCADE',
+      nullable: true,
+    },
+  )
+  @JoinColumn({ name: 'participant' })
+  participant?: Participant;
 }
--- src/results/result.module.ts
+++ src/results/result.module.ts
@@ -4,9 +4,9 @@ import { ResultController } from './result.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Result } from './entities/result.entity';
 import { Participant } from 'src/participants/entities/participant.entity';
-import { Lot } from 'src/lots/entities/lot.entity';
 import { LotService } from 'src/lots/lots.service';
 import { ParticipantService } from 'src/participants/participant.service';
+import { Lot } from 'src/lots/entities/lot.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Result, Participant, Lot])],
